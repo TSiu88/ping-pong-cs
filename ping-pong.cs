38255cb7d93e46731a4510fd8f267cea984a0af7
Adela <adela.yohana@gmail.com>
2020-02-24 08:21:34 -0800

    Add a method to gather user input

--- ping-pong.cs
+++ ping-pong.cs
@@ -4,11 +4,14 @@ class PingPong
 {
   static void Main()
   {
-
+    AskUserInput();
+    int userInput = int.Parse(Console.ReadLine());
+    Console.WriteLine(userInput);
   }
 
   static void AskUserInput()
   {
     Console.WriteLine("Enter a number:");
   }
+
 }
